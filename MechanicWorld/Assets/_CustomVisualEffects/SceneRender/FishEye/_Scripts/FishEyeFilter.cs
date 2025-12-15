acf7e1ed0895a93261a820f84bef9c13c0338c4e
MaksimMur <belarusbest@mail.com>
2025-12-15 11:45:16 +0300

    Warping & fish eye filters

---
+++ MechanicWorld/Assets/_CustomVisualEffects/SceneRender/FishEye/_Scripts/FishEyeFilter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishEyeFilter : SimpleFilter
+{
+    [SerializeField]
+    private float _pow = 1.5f;
+
+    protected override void OnUpdate()
+    {
+        _mat.SetFloat("_BarrelPower", _pow);
+    }
+}
---
+++ MechanicWorld/Assets/_CustomVisualEffects/SceneRender/Warping/_Scripts/WarpingFilter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpingFilter : SimpleFilter
+{
+    [Range(1,10)]
+    [SerializeField]
+    private float _amplitude = 5;
+    [Range(1, 50)]
+    [SerializeField]
+    private float _speed = 15;
+
+    protected override void OnUpdate()
+    {
+        _mat.SetFloat("_Amplirude", _amplitude);
+        _mat.SetFloat("Speed", _speed);
+    }
+}
